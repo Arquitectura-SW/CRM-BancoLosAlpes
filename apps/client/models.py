c578f1d9f187f915b1eac0b6e8fc2d222b1b2af8
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-04-07 18:42:33 -0500

    Modified client model

--- apps/client/models.py
+++ apps/client/models.py
@@ -10,6 +10,7 @@ class Client(models.Model):
     
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    id_number = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     address = models.TextField()
