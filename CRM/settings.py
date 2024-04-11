9050db09b3eca390a899f4abe3dbc2fabf31d53b
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-04-11 15:07:12 -0500

    Created connection to postgres

--- CRM/settings.py
+++ CRM/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,9 +71,17 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}, 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_db',
+        'USER': 'admin',
+        'PASSWORD': 'isis2503',
+        'HOST': '10.128.0.51',
+        'PORT': '5432',
     }
 }
 
--- apps/transactions/serializer.py
+++ apps/transactions/serializer.py
@@ -5,4 +5,3 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
-        
