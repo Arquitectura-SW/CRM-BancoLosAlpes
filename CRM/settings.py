d3a699b0d8f06b02e2bc0048e188198871228b5a
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-04-11 15:41:49 -0500

    Set allowed hosts

--- CRM/settings.py
+++ CRM/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-(#k)r&dpu$7^!7o+m#u&segafdq%iqgte65jrru16%u#&vg36e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
