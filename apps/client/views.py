76c3cbb3d5617b5709bb7146cedba1bf036d0b2a
SofRozo <as.rozo@uniandes.edu.co>
2024-04-12 09:56:31 -0500

    verification

--- apps/client/views.py
+++ apps/client/views.py
@@ -13,6 +13,10 @@ def client(request):
         serializer = ClientSerializer(clients, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
+        #add verification of user, if the user is not legal and the digits of the cedula are not 10, return a message
+        is_valid, message = validate_client_data(request.data)
+        if not is_valid:
+            return Response({'detail': message}, status=status.HTTP_400_BAD_REQUEST)
         client = create_client(request.data)
         serializer = ClientSerializer(client)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -25,6 +29,10 @@ def client_by_id(request, id_or_id_number):
             serializer = ClientSerializer(client)
             return Response(serializer.data)
         elif request.method == 'PUT':
+            #add verification of user, if the user is not legal and the digits of the cedula are not 10, return a message
+            is_valid, message = validate_client_data(request.data)
+            if not is_valid:
+                return Response({'detail': message}, status=status.HTTP_400_BAD_REQUEST)
             client = update_client(id_or_id_number, request.data)
             serializer = ClientSerializer(client)
             return Response(serializer.data)
@@ -33,4 +41,24 @@ def client_by_id(request, id_or_id_number):
             return Response(status=status.HTTP_204_NO_CONTENT)
     except Exception as e:
         return Response(e.args[0], status=e.args[1])
+    
+# Auxiliary function 
+def validate_client_data(client_data):
+    birth_date_str = client_data.get('birth_date', None)
+    if birth_date_str:
+        birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d').date()
+        today = datetime.now().date()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+        if age < 18:
+            return False, "The client must be of legal age to register in the system."
+    else:
+        return True, None
+
+    id_number = client_data.get('id_number', None)
+    if id_number:
+        if len(id_number) != 10:
+            return False, "The document number (cedula) must have 10 digits."
+
+    return True, None
+
 
