0bce3bc34a83ebf266a9c270471f88453e901d22
SofRozo <as.rozo@uniandes.edu.co>
2024-04-11 19:38:19 -0500

    modifications

--- apps/client/urls.py
+++ apps/client/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('clients/', views.client),
     path('clients/<str:id_or_id_number>/', views.client_by_id),
+    path('clients/check_or_create/', views.client_check_or_create), 
 ]
--- apps/client/views.py
+++ apps/client/views.py
@@ -35,4 +35,33 @@ def client_by_id(request, id_or_id_number):
             return Response(status=status.HTTP_204_NO_CONTENT)
     except Exception as e:
         return Response(e.args[0], status=e.args[1])
+    
+@api_view(['POST'])
+def client_check_or_create(request):
+    try:
+        if request.method == 'POST':
+            id_or_id_number = request.data.get('id_number', None)
+
+            if id_or_id_number:
+                if id_or_id_number.isdigit():
+                    client = get_client_by_id(int(id_or_id_number))
+                else:
+                    client = get_client_by_id_number(id_or_id_number)
+
+                if client:
+                    client_data = request.data
+                    for key, value in client_data.items():
+                        setattr(client, key, value)
+                    client.save()
+                    serializer = ClientSerializer(client)
+                    return Response(serializer.data)
+                else:
+                    new_client = create_client(request.data)
+                    serializer = ClientSerializer(new_client)
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"error": "The client's ID was not given."}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response(e.args[0], status=e.args[1])
+
 
