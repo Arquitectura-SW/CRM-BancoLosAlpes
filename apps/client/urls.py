61fcb3ff8714899d8d3a273f787e74d13be3a6cb
SofRozo <as.rozo@uniandes.edu.co>
2024-04-12 08:59:01 -0500

    .

--- apps/client/urls.py
+++ apps/client/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('clients/', views.client),
-    path('clients/<str:id_or_id_number>/', views.client_by_id_number),
+    path('clients/<str:id_or_id_number>/', views.client_by_id),
 ]
