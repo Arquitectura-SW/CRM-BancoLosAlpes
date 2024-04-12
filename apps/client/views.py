b041f69b6129fd4223f6ce3c2ee916e044eac2d8
SofRozo <as.rozo@uniandes.edu.co>
2024-04-12 10:26:30 -0500

    change

--- apps/client/views.py
+++ apps/client/views.py
@@ -48,15 +48,8 @@ def validate_client_data(client_data):
     birth_date_str = client_data.get('birth_date', None)
     message = ''
     is_valid = True
-
-    if not id_number:
-        message += "The document number (cedula) field cannot be null. "
-        is_valid = False
-    else:
-        if len(id_number) != 10:
-            message += "The document number (cedula) must have 10 digits. "
-            is_valid = False
-
+    
+    if id_number:
         client = get_client_by_id_number(id_number)
         if client:
             message += "The client with the provided document number already exists in the system. "
@@ -68,6 +61,12 @@ def validate_client_data(client_data):
         age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
         if age < 18:
             message += "The client must be of legal age to register in the system. "
+            is_valid = False
+
+    id_number = client_data.get('id_number', None)
+    if id_number:
+        if len(id_number) != 10:
+            message += "The document number (cedula) must have 10 digits."
             is_valid = False
 
     return is_valid, message if message else None
